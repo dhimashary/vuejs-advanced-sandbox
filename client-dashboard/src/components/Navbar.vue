1cdbaf40f00497aeb33a6e5b958a6ee188acaaed
dhimashary <dhimas.hary@gmail.com>
2021-06-03 13:09:10 +0700

    3.programmatic-navigation

---
+++ client-dashboard/src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <a href="#" @click="logout">Logout</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  methods: {
+    logout () {
+      localStorage.clear()
+      // push to login
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ client-dashboard/src/views/Login.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is LOGIN Page</h1>
+  </div>
+</template>

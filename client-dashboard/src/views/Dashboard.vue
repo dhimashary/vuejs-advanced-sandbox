26744034615a1c4fa7b7d9805f85e341209ec612
dhimashary <dhimas.hary@gmail.com>
2021-06-03 14:01:34 +0700

    6.nav-guard

---
+++ client-dashboard/src/views/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>This is Dashboard Page</h1>
+    <p>
+      Tambahkan beforeRouteEnter yang akan mengecek apakah user memiliki access_token / tidak di localStorage pada Views "Dashboard", jika user tidak memiliki access_token alihkan ke halaman login
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard'
+}
+</script>
